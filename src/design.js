05ef3a72941e41f3b2ba0895a3fd2807078e894c
Mikael Priuska <mikael.priuska@rebooted.solutions>
2026-02-27 14:22:09 +0200

    Template functions for the project data

---
+++ src/design.js
@@ -0,0 +1,41 @@
+
+function loadProducts() {}
+
+function loadCustomers() {}
+
+function loadOrders() {}
+
+
+function validateProduct(product) {}
+
+function validateCustomer(customer) {}
+
+function validateOrder(order) {}
+
+
+
+function calculateSalesSummary(orders, products, timeframe) {}
+
+function calculateRevenue(orders, products) {}
+
+function calculateOrderCounts(orders) {}
+
+
+function getTopSellingProducts(orders, products, timeframe) {}
+
+function orderTopSellingProducts(topSellingProducts) {}
+
+function getTopSellingProductsByCategory(orders, products, timeframe, category) {} // Depends on customer needs
+
+
+function getUserPurchasesByTimeframe(orders, customers, timeframe) {}
+
+function getUserPurchases(orders, customers) {}
+
+
+function calculateLowStock(products, threshold) {}
+
+
+
+
+
